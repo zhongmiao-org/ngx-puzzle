107030495986e45dddd6a6bb5135d9646936ad1f
ark-65 <liuwufangzhou@vip.qq.com>
2025-09-15 15:29:50 +0800

    feat: table

--- projects/puzzle/src/core/constants/table-list.ts
+++ projects/puzzle/src/core/constants/table-list.ts
@@ -2,7 +2,7 @@ import { BaseSelectOption } from '../interfaces';
 import { TableTypesEnum } from '../enums';
 
 export const TABLE_TYPE_OPTIONS: BaseSelectOption<TableTypesEnum>[] = [
-  { label: '普通表格', val: TableTypesEnum.default },
+  // { label: '普通表格', val: TableTypesEnum.default },
   { label: '聚合分组', val: TableTypesEnum.pivotTable },
   // { label: '行分组表格', val: TableTypesEnum.rowGrouping },
   // { label: '数据透视表', val: TableTypesEnum.pivoting },
